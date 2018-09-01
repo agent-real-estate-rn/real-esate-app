5f6717129c356d8defc527069e1b56e01e5bc849
Bobby Lee <bobby.h.lee@gmail.com>
2018-09-01 18:28:52 +0700

    commiting to loginmod

--- src/actions/login.js
+++ src/actions/login.js
@@ -2,25 +2,79 @@ import firebase from '../firebase';
 
 export default function mapDispatchToLoginProps(dispatch) {
   return {
-    loginByUsernamePassword: (username, password) => {
+    loginByUsernamePassword: (username, password, navigation) => {
       dispatch({
         type: 'LOGIN_ING'
       })
-      return loginBusiness(dispatch)(username, password);
+      return loginBusiness(dispatch)(username, password, navigation);
     },
-    loginByFacebook: () => {
+
+    loginByFacebook: async () => {
+      const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('1952254625024149', {
+        permissions: ['public_profile'],
+      });
+      
+      if (type === 'success') {
+        // Get the user's name using Facebook's Graph API
+        const response = await fetch(
+          `https://graph.facebook.com/me?access_token=${token}`);
+        const data = await response.json();
+        console.log('this is response data: ', data);
+
+        let credential = firebase.auth.FacebookAuthProvider.credential(token);
+        console.log('this is credential: ', credential)
+        firebase.auth().signInAndRetrieveDataWithCredential(credential).catch((error) => {
+          console.log(err);
+        });
+
+      dispatch({
+        type: 'LOGIN_SUCCESS',
+        uid: data.id,
+        userInfo: data.name,
+      })
 
     }
+
+    },
+
+    loginAuto: (navigation) => {
+
+      firebase.auth().onAuthStateChanged((user) => {
+        console.log('this is autologin user: ', user);
+        const data = user;
+        dispatch({
+          type: 'LOGIN_SUCCESS',
+          uid: data.uid,
+          userInfo: data.email,
+        })
+        if (user != null) {
+          navigation.navigate('Tabs');
+        }
+      });
+
+
+    },
+
   }
 }
 
-const loginBusiness = (dispatch) => (username, password) => {
-  firebase.auth().signInWithEmailAndPassword(username, password).then(data => {
-    dispatch({
-      type: 'LOGGED_IN',
-      user: data.user.uid
-    })
-  });
+const loginBusiness = (dispatch) => (username, password, navigation) => {
+  console.log('this is password: ', password);
+  
+    try {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(username, password).then(data => {
+          dispatch({
+            type: 'LOGIN_SUCCESS',
+            uid: data.user.uid,
+            userInfo: data.user.userInfo,
+          })
+          navigation.navigate("Tabs");
+        });
+    } catch (error) {
+      console.log(error.toString());
+    }
 }
 
 // example of action.
--- src/stores/login.js
+++ src/stores/login.js
@@ -1,12 +1,15 @@
-const initialState = {isLoading: false, uid: '', userInfo};
+const initialState = {isLoading: false, uid: '', userInfo:''};
 
 export default login = (state= initialState, action) => {
   switch (action.type) {
     case 'LOGIN_ING': {
       return {...state, isLoading: true};
     }
-    case 'Login_SUCCESS': {
-      return {isLoading: false, uid: action.uid, userInfo: action.userInfo};
+    case 'LOGIN_SUCCESS': {
+      // return state;
+      console.log('this is the state: ', state);
+      
+      return {...state, isLoading: false, uid: action.uid, userInfo: action.userInfo};
     }
     default:
       return state;
